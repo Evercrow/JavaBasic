7eef91daa7ab165d68a1b1b9a1a44f5d9ef41c49
Anton Sizykh <toxa-v-s@yandex.ru>
2023-03-04 14:22:15 +0300

    lesson 7 and pom.xml

---
+++ src/main/java/Lesson4/Homework4.java
@@ -0,0 +1,25 @@
+package Lesson4;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Реализовать алгоритм пирамидальной сортировки (HeapSort)
+ */
+public class Homework4 {
+    public static void main(String[] args) {
+
+        int[] testArray = {13,2,6,43,20,1,1,7,6};
+
+        System.out.println();
+
+    }
+
+
+    public static int[] pyramidSort(int[] arr){
+
+        return  arr;
+    }
+
+}
